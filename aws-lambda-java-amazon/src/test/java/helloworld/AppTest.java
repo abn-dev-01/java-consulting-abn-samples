0b1a2ece90d8f55ced21e4d764c3d513c6b60b67
Andrew.Nikitin <abn.dev.01@gmail.com>
2023-02-12 23:49:33 +0300

    :aws-lambda-java-amazon project

---
+++ aws-lambda-java-amazon/src/test/java/helloworld/AppTest.java
@@ -0,0 +1,22 @@
+package helloworld;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void successfulResponse() {
+        App app = new App();
+        APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+        assertEquals(200, result.getStatusCode().intValue());
+        assertEquals("application/json", result.getHeaders().get("Content-Type"));
+        String content = result.getBody();
+        assertNotNull(content);
+        assertTrue(content.contains("\"message\""));
+        assertTrue(content.contains("\"hello world\""));
+        assertTrue(content.contains("\"location\""));
+    }
+}
